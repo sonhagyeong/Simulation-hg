600bcdc9cc82a7ef5502b146e9cdbd36b91ba43f
ppurify <qpqp8558@gmail.com>
2022-11-23 12:55:37 +0900

    when object hit own destination, it is destoried

---
+++ School/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/DestoryObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryObject : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
