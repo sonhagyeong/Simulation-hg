4b9d174b0eba6f91b8d98afcb646b9f88d1f8c1d
ppurify <qpqp8558@gmail.com>
2022-12-01 01:32:00 +0900

    keep going

--- School/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/CreateCar.cs
+++ School/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/CreateCar.cs
@@ -9,56 +9,101 @@ namespace TrafficSimulation{
     {   
         public float Y_Position = 1.5f;
         // 뽑을 Segment
-        // public GameObject Segment;
+        public GameObject segment;
         // 뽑을 트럭
         public GameObject Truck;
         // Segment별 Position
-        public Vector3 segment_position;
-        // Truck Position
-        public Vector3 Truck_position;
-        private TrafficSystem trafficSystem = null;
-        
+        public Vector3 path_Position;
+        public Quaternion path_Rotation;
 
+        string selectedPath;
+        string nameTag;
+
+        List<string> pathName = new List<string>();
+
+        Dictionary<string, string> tagsDic = new Dictionary<string, string>();
+        // Dictionary<string, Vector3> pathPositionsDic = new Dictionary<string, Vector3>();
+        Dictionary<string, Quaternion> rotationsDic = new Dictionary<string, Quaternion>();
+        
+        
+        
+        
         // Start is called before the first frame update
         void Start()
         {   
-            GetSegmentPoint();
+            AddPathName();
+            AddTags();
+            AddRotation();
+            // AddPosition();
+            GetPath();
+            GetPathPosition();
+            GetPathRotation();
             // Create();
         }
 
-        // 뽑은 Segment position 얻기
-        void GetSegmentPoint()
-        {   
-            List<TrafficSystem> trafficSystem_list = new List<TrafficSystem>();
-            trafficSystem = FindObjectOfType<TrafficSystem>();
-            // foreach(TrafficSystem ts in trafficSystem)
-            // {
-            //     trafficSystem_list.AddRange(ts);
-            // }
-            // Debug.Log(trafficSystem.name);
-            // for(TrafficSystem i; i in trafficSystem;)             
-            // {
-            //     Debug.Log("trafficSystem : "+ i);
-            // }
-            // Debug.Log("traffic system count :" +trafficSystem);
-            // Debug.Log("TrafficSystem : " + FindObjectOfType<TrafficSystem>());
-
-            // trafficSystem = GameObject.Find("Path1") as TrafficSystem;
-            // Truck.GetComponent<VehicleAI>().trafficSystem = trafficSystem;
-            
-            
-            // segment_position = Segment.transform.position;
-            // Debug.Log("segment point : "+ segment_position);
+        void AddPathName()
+        {
+            pathName.Add("path_0");
+            pathName.Add("path_1");
+            pathName.Add("path_2");
+            pathName.Add("path_3");
+        }
+        
+        void AddTags()
+        {
+            //path에 tag는 string 값으로 추가
+            tagsDic.Add(pathName[0], "place1");
+            tagsDic.Add(pathName[1], "place2");
+            tagsDic.Add(pathName[2], "place3");
+            tagsDic.Add(pathName[3], "place4");
+        }
+    
+        void AddRotation()
+        {
+            //path종류는 String, 좌표는 V3 값으로 추가
+            rotationsDic.Add(pathName[0], Quaternion.Euler(0, 0, 0));
+            rotationsDic.Add(pathName[1], Quaternion.Euler(0, 180, 0));
+            rotationsDic.Add(pathName[2], Quaternion.Euler(0, 180, 90));
+            rotationsDic.Add(pathName[3], Quaternion.Euler(0, 0, 0));
         }
 
+        // void AddPosition()
+        // {
+        //     //path종류는 String, 좌표는 V3 값으로 추가
+        //     pathPositionsDic.Add(pathName[0], GameObject.Find(pathName[0]).transform.position);
+        //     pathPositionsDic.Add(pathName[1], GameObject.Find(pathName[1]).transform.position);
+        //     pathPositionsDic.Add(pathName[2], GameObject.Find(pathName[2]).transform.position);
+        //     pathPositionsDic.Add(pathName[3], GameObject.Find(pathName[3]).transform.position);
+        // }
+
+        void GetPath()
+        {
+            int randomNum = Random.Range(0, 4);
+            selectedPath = pathName[randomNum];
+            // Debug.Log("selected path : " + selectedPath);
+        }
+
+        // 뽑은 Segment position 얻기
+        void GetPathPosition()
+        {   
+            path_Position = GameObject.Find(selectedPath).transform.position;
+            // Debug.Log("path_Position : "+ path_Position);
+        }
+
+        void GetPathRotation()
+        {
+            path_Rotation = rotationsDic[selectedPath];
+        }
+
+        void GetTag()
+        {
+            nameTag = tagsDic[selectedPath];
+        }
         // Truck 인스턴스화 하기
         void Create()
         {   
-            // Segment-5일경우
-            Truck_position = new Vector3(-43.16f, 1.5f, 35.01f);
-            // Truck.GetComponent<VehicleAI>().trafficSystem = GameObject.Find("Path1") as trafficSystem;
-            // Truck_trafficSystem = GameObject.Find("Path1");
-            Instantiate(Truck, Truck_position, Quaternion.Euler(0, 180, 0));
+            Truck.GetComponent<VehicleAI>().trafficSystem = FindObjectOfType<TrafficSystem>();
+            Instantiate(Truck, path_Position, path_Rotation);
             
             Debug.Log("Truck traffic system is " + Truck.GetComponent<VehicleAI>().trafficSystem);
         }
---
+++ School/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/PopVehicle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class PopVehicle : MonoBehaviour
+{
+    // Stopwatch stopwatch = new Stopwatch();
+    // public void OnCollisionEnter(Collision collision){
+
+    //   if (collision.gameObject.tag == "Target"){
+    //     gameObject.SetActive(false);
+    //     stopwatch.Stop();
+    //     float time = stopwatch.ElapsedMilliseconds ;
+    //     UnityEngine.Debug.Log(time*0.001);
+
+    //     FileStream fs = new FileStream("Assets/result.csv", FileMode.Append, FileAccess.Write);
+    //     StreamWriter streamWriter = new StreamWriter(fs, System.Text.Encoding.Unicode);
+
+    //     var list = new List<string>();
+    //     string name = this.gameObject.ToString();
+    //     list.Add((time*0.001).ToString());
+        
+    //     for (int i = 0; i < list.Count; i++)
+    //     {
+    //         var tmp = list[i];
+
+    //         //Debug.Log(string.Format("{0},{1}", tmp.nIndex, tmp.sName));
+    //         streamWriter.WriteLine(string.Format("{0},{1:F4}", name.Substring(0, name.Length - 24), tmp));
+    //     }
+
+    //     streamWriter.Close();
+    //   }
+    //   else {
+    //     streamWriter.Start();
+    //   }
+    // }
+    
+}
