5780445af86b83009feae075a447c589788f0912
ppurify <qpqp8558@gmail.com>
2022-11-30 21:07:46 +0900

    have to edit findobjectoftype

--- School/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/CreateCar.cs
+++ School/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/CreateCar.cs
@@ -3,34 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-namespace TrafficSimulation {
+namespace TrafficSimulation{
 
     public class CreateCar : MonoBehaviour
     {   
         public float Y_Position = 1.5f;
         // 뽑을 Segment
-        public GameObject Segment;
+        // public GameObject Segment;
         // 뽑을 트럭
         public GameObject Truck;
         // Segment별 Position
         public Vector3 segment_position;
         // Truck Position
         public Vector3 Truck_position;
-        // public TrafficSystem trafficSystem; 
+        private TrafficSystem trafficSystem = null;
         
 
         // Start is called before the first frame update
         void Start()
         {   
             GetSegmentPoint();
-            Create();
+            // Create();
         }
 
         // 뽑은 Segment position 얻기
         void GetSegmentPoint()
         {   
-            segment_position = Segment.transform.position;
-            Debug.Log("segment point : "+ segment_position);
+            List<TrafficSystem> trafficSystem_list = new List<TrafficSystem>();
+            trafficSystem = FindObjectOfType<TrafficSystem>();
+            // foreach(TrafficSystem ts in trafficSystem)
+            // {
+            //     trafficSystem_list.AddRange(ts);
+            // }
+            // Debug.Log(trafficSystem.name);
+            // for(TrafficSystem i; i in trafficSystem;)             
+            // {
+            //     Debug.Log("trafficSystem : "+ i);
+            // }
+            // Debug.Log("traffic system count :" +trafficSystem);
+            // Debug.Log("TrafficSystem : " + FindObjectOfType<TrafficSystem>());
+
+            // trafficSystem = GameObject.Find("Path1") as TrafficSystem;
+            // Truck.GetComponent<VehicleAI>().trafficSystem = trafficSystem;
+            
+            
+            // segment_position = Segment.transform.position;
+            // Debug.Log("segment point : "+ segment_position);
         }
 
         // Truck 인스턴스화 하기
