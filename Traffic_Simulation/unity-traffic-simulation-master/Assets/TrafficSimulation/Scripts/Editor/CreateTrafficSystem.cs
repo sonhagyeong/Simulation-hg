d6e9920453e102a3a100c007581bbadc9ae98f4f
ppurify <qpqp8558@gmail.com>
2023-05-18 17:47:03 +0900

    if there doesn't exist any traffic system, make it

---
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/Editor/CreateTrafficSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace TrafficSimulation {
+    public class CreateTrafficSystem : EditorWindow
+    {   
+        private static string routeFilePath = "C:\\Users\\USER\\workspace\\RoutePoints.csv";
+        private static string intersectionFilePath = "C:\\Users\\USER\\workspace\\IntersectionPoints.csv";
+
+        private static List<List<Vector3>> routes = new List<List<Vector3>>();
+        private static List<Vector3> intersections = new List<Vector3>();
+        
+        [InitializeOnLoadMethod]
+        private static void PlayModeState()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.EnteredPlayMode)
+            {
+                CreateSystem();
+            }
+        }
+
+        private static void CreateSystem()
+        {
+            if(GameObject.FindObjectOfType<TrafficSystem>() == null)
+            {   
+                Debug.Log("Creating Traffic System");
+                FileWindow.CreateAll(routeFilePath, intersectionFilePath, routes, intersections);
+            }
+
+            else
+            {
+                Debug.Log("Traffic System already exists");
+            }
+        }
+        
+        
+    }
+}
