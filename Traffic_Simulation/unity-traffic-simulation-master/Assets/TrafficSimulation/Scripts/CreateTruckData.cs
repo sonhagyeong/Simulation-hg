6471aef4e22a44da23a2d265f15a3919f6061c9a
ppurify <qpqp8558@gmail.com>
2023-05-18 17:45:46 +0900

    Save truck information

---
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/CreateTruckData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTruckData : MonoBehaviour
+{
+    public string Name { get; private set; }
+    public string Route { get; private set; }
+
+    public void CreateData(string name, string route)
+    {
+        Name = name;
+        Route = route;
+    }
+}
