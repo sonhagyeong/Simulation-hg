e61c9b493c8970b8cd73e59200cee57473310bb4
ppurify <qpqp8558@gmail.com>
2023-05-19 16:19:53 +0900

    check time to destination

---
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/Destination.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+
+public class Destination : MonoBehaviour
+{   
+    private Stopwatch watch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        watch = new Stopwatch();
+        watch.Start();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {   
+        if(other.name == "Destination")
+        {
+            watch.Stop();
+            float elapsedSeconds = watch.ElapsedMilliseconds / 1000f;
+            UnityEngine.Debug.Log("Time elapsed: " + elapsedSeconds + " s");
+        }
+    }
+}
