a3829daf5faf1b56936210b0af8c0ae5d638c5c6
ppurify <qpqp8558@gmail.com>
2023-05-22 17:19:12 +0900

    to exit when trucks are done

---
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/ExitPlayMode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ExitPlayMode : MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+#if UNITY_EDITOR
+        // Check if there are no objects in the hierarchy starting with "Truck"
+        bool noTruckObjects = true;
+        foreach (Transform obj in GameObject.FindObjectsOfType<Transform>())
+        {
+            if (obj.name.StartsWith("Truck"))
+            {
+                noTruckObjects = false;
+                break;
+            }
+        }
+
+        // Exit play mode if there are no Truck objects
+        if (noTruckObjects)
+        {
+            EditorApplication.ExitPlaymode();
+        }
+#endif
+    }
+}
