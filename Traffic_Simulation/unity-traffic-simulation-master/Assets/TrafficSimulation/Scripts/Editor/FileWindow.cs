64fbb007298300142b54bdf8c995cc73393424e9
ppurify <qpqp8558@gmail.com>
2023-05-23 23:01:44 +0900

    Edit corner position

--- Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/Editor/FileWindow.cs
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/Editor/FileWindow.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System;
+
 namespace TrafficSimulation {    
     public class FileWindow : EditorWindow
     {   
@@ -23,8 +25,18 @@ namespace TrafficSimulation {
         private static List<Vector3> newPoints;
         private static Vector3 placeSize = new Vector3(20,10,10);
         private static float placePos_y = placeSize.y/2;
+        private static List<Vector3> checkingRotateList;
+        private static bool isRotate;
+        private static List<Vector3> cornerPositions = new List<Vector3>{new Vector3(0,0,0), new Vector3(1350,0,0), new Vector3(1350,0,600), new Vector3(0,0,600)};
+        
+        private static float x1;
+        private static float x2;
+        private static float x3;
 
-
+        private static float z1;
+        private static float z2;
+        private static float z3;
+        
         
         [MenuItem("Component/Traffic Simulation/File Window")]
         public static void ShowWindow()
@@ -76,14 +88,30 @@ namespace TrafficSimulation {
             EditorGUILayout.BeginHorizontal();
             if (GUILayout.Button("Create Routes in Scene", GUILayout.Width(150)))
             {   
-                routes = CreateRouteList(routefilePath, routes);
-                CreateRoutes(routes);
+                if(routefilePath != null)
+                {
+                    routes = CreateRouteList(routefilePath, routes);
+                    CreateRoutes(routes, cornerPositions, intersections);
+                }
+                
+                else
+                {
+                    Debug.LogError("Check route file Path");
+                }
             }
             GUILayout.Space(10);
             if (GUILayout.Button("Create Intersections in Scene", GUILayout.Width(200)))
             {   
-                intersections = CreateIntersectionList(intersectionfilePath, intersections);
-                CreateIntersections(intersections);
+                if(intersectionfilePath != null)
+                {
+                    intersections = CreateIntersectionList(intersectionfilePath, intersections);
+                    CreateIntersections(intersections);
+                }
+                
+                else
+                {
+                    Debug.LogError("Check intersection file Path");
+                }
             }
             EditorGUILayout.EndHorizontal();
             EditorGUILayout.Space(10);
@@ -108,7 +136,15 @@ namespace TrafficSimulation {
             GUILayout.BeginVertical();
             if (GUILayout.Button("Create All"))
             {
-                CreateAll(routefilePath, intersectionfilePath, routes, intersections);
+                if(routefilePath != null && intersectionfilePath != null)
+                {
+                    CreateAll(routefilePath, intersectionfilePath, routes, intersections);
+                }
+
+                else
+                {
+                    Debug.LogError("Check files Path");
+                }
                 // Close();
             }
             GUILayout.EndVertical();
@@ -180,20 +216,20 @@ namespace TrafficSimulation {
                 string line = reader.ReadLine();
                 string[] fields;
 
-                int currentIntersection = -1;
+                // int currentIntersection = -1;
                 
                 while(line != null)
                 {
                     fields = line.Split(',');
 
-                    if(fields.Length == 4)
+                    if(fields.Length == 3)
                     {
-                        int intersectionNum = int.Parse(fields[3]);
+                        // int intersectionNum = int.Parse(fields[3]);
 
-                        if(intersectionNum != currentIntersection)
-                        {
-                            currentIntersection = intersectionNum;
-                        }
+                        // if(intersectionNum != currentIntersection)
+                        // {
+                        //     currentIntersection = intersectionNum;
+                        // }
 
                         float x = float.Parse(fields[0]);
                         float y = float.Parse(fields[1]);
@@ -265,15 +301,27 @@ namespace TrafficSimulation {
             intersections = CreateIntersectionList(intersectionfilePath, intersections);
             CreateIntersections(intersections);
             routes = CreateRouteList(routefilePath, routes);
-            CreateRoutes(routes);
+            CreateRoutes(routes, cornerPositions, intersections);
         }
 
-        private static void CreateRoutes(List<List<Vector3>> routes)
+        private static void CreateRoutes(List<List<Vector3>> routes, List<Vector3> corners, List<Vector3> intersections)
         {
             EditorHelper.SetUndoGroup("Create Routes");
 
             GameObject places = EditorHelper.CreateGameObject("Places");
             places.transform.position = Vector3.zero;
+
+            if(corners.Count > 0 && intersections.Count > 0)
+            {
+                checkingRotateList = new List<Vector3>();
+                checkingRotateList.AddRange(corners);
+                checkingRotateList.AddRange(intersections);
+            }
+            
+            else
+            {
+                Debug.LogError("There is no corners or intersections");
+            }
 
             for(int i=0; i<routes.Count; i++)
             {   
@@ -293,49 +341,87 @@ namespace TrafficSimulation {
                 EditorHelper.BeginUndoGroup("Add Segment", wps);
 
                 AddSegment(route[0], routeName);
-                // AddSegment(Vector3.zero, routeName);
-
-
-                // foreach(Vector3 point in route)
-                // {  
-                //     Vector3 newPoint = point;
-                //     newPoint.y = route_Pos_y;
-                //     AddWaypoint(newPoint);
-                // }
 
                 for(int p=0; p <route.Count; p++)
                 {  
-                    if(p+1 < route.Count)
-                    {   
-                        Vector3 nowPosition = route[p];
-                        if(nowPosition == Vector3.zero)
+                    List<Vector3> newRotationPoints = new List<Vector3>();
+                    
+                    if(p > 0 && p+1 < route.Count)
+                    {
+                        if(RotatePosition(route[p], checkingRotateList))
                         {
-                            Debug.Log("nowPosition is zero");
-                            nowPosition.x = 25f;
-                            route[p] = nowPosition;
-                            Debug.Log("changed nowPosition : " + route[p]);
+                            Debug.Log(">>> Rotate nowPosition : "+  route[p]);
+                            newRotationPoints = ChangeToRotate(route[p-1], route[p], route[p+1]);
+                        
+                            foreach(Vector3 rPoint in newRotationPoints)
+                            {   
+                                Debug.Log(" rPoint : " + rPoint);
+                                Vector3 newRoPoint = rPoint;
+                                newRoPoint.y = route_Pos_y;
+                                AddWaypoint(newRoPoint);
+                            }
                         }
-                        newPoints = EditRoutePositions(route[p], route[p+1]);
-                        route[p+1] = newPoints[1];
-
-                        Vector3 newPoint = newPoints[0];
-                        newPoint.y = route_Pos_y;
-                        AddWaypoint(newPoint);
                     }
 
                     else
-                    {   
-                        Vector3 newPoint = newPoints[1];
+                    {
+                        Vector3 newPoint = route[p];
                         newPoint.y = route_Pos_y;
                         AddWaypoint(newPoint);
 
-                        GameObject place = EditorHelper.CreateGameObject("place-" + i, places.transform);
-                        place.transform.position = newPoints[1];
-                        if(place.GetComponent<Collider>() == null)
+                        if(p+1 == route.Count)
                         {
-                            AddCollider(place);
+                            GameObject place = EditorHelper.CreateGameObject("place-" + i, places.transform);
+                            place.transform.position = newPoint;
+                            if(place.GetComponent<Collider>() == null)
+                            {
+                                AddCollider(place);
+                            }   
                         }
                     }
+
+                    // Vector3 newPoint = route[p];
+                    // newPoint.y = route_Pos_y;
+                    // AddWaypoint(newPoint);
+
+                    // if(p+1 < route.Count)
+                    // {   
+                        // Vector3 nowPosition = route[p];
+                        // if(nowPosition == Vector3.zero)
+                        // {
+                        //     Debug.Log("nowPosition is zero");
+                        //     nowPosition.x = 25f;
+                        //     route[p] = nowPosition;
+                        //     Debug.Log("changed nowPosition : " + route[p]);
+                        // }
+
+                        
+
+                        // Vector3 newPoint = route[p];
+                        // newPoint.y = route_Pos_y;
+                        // AddWaypoint(newPoint);
+
+                        // newPoints = EditRoutePositions(route[p], route[p+1]);
+                        // route[p+1] = newPoints[1];
+
+                        // Vector3 newPoint = newPoints[0];
+                        // newPoint.y = route_Pos_y;
+                        // AddWaypoint(newPoint);
+                    // }
+
+                    // else
+                    // {   
+                    //     Vector3 newPoint = newPoints[1];
+                    //     newPoint.y = route_Pos_y;
+                    //     AddWaypoint(newPoint);
+
+                    //     GameObject place = EditorHelper.CreateGameObject("place-" + i, places.transform);
+                    //     place.transform.position = newPoints[1];
+                    //     if(place.GetComponent<Collider>() == null)
+                    //     {
+                    //         AddCollider(place);
+                    //     }
+                    // }
                 }
 
             }
@@ -431,6 +517,191 @@ namespace TrafficSimulation {
             center.y = placePos_y;
             bc.center = center;
             bc.isTrigger = true;
+        }
+
+        private static bool RotatePosition(Vector3 nowPosition, List<Vector3> coordinateList)
+        {   
+            isRotate = CheckIfCoordinateExists(nowPosition, coordinateList);
+            return isRotate;
+        }
+        
+        private static bool CheckIfCoordinateExists(Vector3 coordinate, List<Vector3> coordinateList)
+        {
+            return coordinateList.Contains(coordinate);
+        }
+
+        private static List<Vector3> ChangeToRotate(Vector3 previousPoint, Vector3 nowPoint, Vector3 nextPoint)
+        {   
+            Debug.Log("previousPoint : " + previousPoint);
+            Debug.Log("nowPoint : " + nowPoint);
+            Debug.Log("nextPoint : " + nextPoint);
+
+            float nowPoint_x = nowPoint.x;
+            float nowPoint_z = nowPoint.z;
+
+            float axis_x_pre_now = previousPoint.x - nowPoint_x;
+            float axis_x_next_now = nextPoint.x - nowPoint_x;
+
+            float axis_z_pre_now = previousPoint.z - nowPoint_z;
+            float axis_z_next_now = nextPoint.z - nowPoint_z;
+
+            List<Vector3> rotatePoints = new List<Vector3>();
+
+            float Num_1 = 15f;
+            float Num_2 = Num_1 * 2;
+
+            // float x1;
+            // float x2;
+            // float x3;
+
+            // float z1;
+            // float z2;
+            // float z3;
+
+            // case 1
+            if(axis_x_next_now == 0 && axis_z_pre_now == 0)
+            {
+                // To up
+                if(axis_z_next_now > 0)
+                {   
+                    if(axis_x_pre_now < 0)
+                    {   
+                        Debug.Log("up left");
+                        x1 = nowPoint_x - Num_2;
+                        x2 = nowPoint_x - Num_1;
+                    }
+
+                    // right
+                    else if(axis_x_pre_now > 0)
+                    {   
+                        Debug.Log("up right");
+                        x1 = nowPoint_x + Num_2;
+                        x2 = nowPoint_x + Num_1;
+                    }
+
+                    x3 = nowPoint_x;
+
+                    z1 = nowPoint_z;
+                    z2 = Calculate_Z_Up(z1, Num_1);
+                    z3 = Calculate_Z_Up(z1, Num_2);
+                    // }
+                }
+
+                // To Down 1
+                else if(axis_z_next_now < 0)
+                {
+                    //left
+                    if(axis_x_pre_now > 0)
+                    {
+                        Debug.Log("case 1 down left");
+                        x1 = nowPoint_x + Num_2;
+                        x2 = nowPoint_x + Num_1;
+                    }
+
+                    //right
+                    else if(axis_x_pre_now < 0)
+                    {
+                        Debug.Log("case 1 down right");
+                        x1 = nowPoint_x - Num_2;
+                        x2 = nowPoint_x - Num_1;
+                    }
+
+                    x3 = nowPoint_x;
+
+                    z1 = nowPoint_z;
+                    z2 = Calculate_Z_Down(z1, Num_1);
+                    z3 = Calculate_Z_Down(z1, Num_2);
+                }
+            }
+
+            // case 2
+            else if(axis_x_pre_now == 0 && axis_z_next_now == 0)
+            {   
+                // To down 2
+                if(axis_z_pre_now > 0 )
+                {
+                    // right
+                    if(axis_x_next_now < 0)
+                    {
+                        Debug.Log("case 2 down right");
+                        x2 = nowPoint_x - Num_1;
+                        x3 = nowPoint_x - Num_2;
+                    }
+
+                    //left
+                    else if(axis_x_next_now > 0)
+                    {
+                        Debug.Log("case 2 down left");
+                        x2 = nowPoint_x + Num_1;
+                        x3 = nowPoint_x + Num_2;
+                    }
+
+                    x1 = nowPoint_x;
+
+                    z3 = nowPoint_z;
+                    z1 = Calculate_Z_Down2(z3, Num_2);
+                    z2 = Calculate_Z_Down2(z3, Num_1);
+                    
+                }
+
+                // To up 2
+                else if(axis_z_pre_now < 0)
+                {   
+                    // right
+                    if(axis_x_next_now > 0)
+                    {
+                        Debug.Log("case 2 up right");
+                        x2 = nowPoint_x + Num_1;
+                        x3 = nowPoint_x + Num_2;
+                    }
+
+                    // left
+                    else if(axis_x_next_now < 0)
+                    {
+                        Debug.Log("case 2 up left");
+                        x2 = nowPoint_x - Num_1;
+                        x3 = nowPoint_x - Num_2;
+                    }
+
+                    x1 = nowPoint_x;
+
+                    z3 = nowPoint_z;
+                    z1 = Calculate_Z_Up2(z3, Num_2);
+                    z2 = Calculate_Z_Up2(z3, Num_1);
+                }
+                
+            }
+            
+
+            Debug.Log(" --- Original rotatePoints Count " + rotatePoints.Count);
+            rotatePoints.Add(new Vector3(x1, 0f, z1));
+            rotatePoints.Add(new Vector3(x2, 0f, z2));
+            rotatePoints.Add(new Vector3(x3, 0f, z3));
+
+            Debug.Log( "rotatePoints: " + rotatePoints[0] + " " + rotatePoints[1] + " " + rotatePoints[2]);
+            return rotatePoints;
+            // }
+     
+        }
+
+        private static float Calculate_Z_Up(float start_z, float uNum)
+        {
+            return start_z + uNum;
+        }
+        
+        private static float Calculate_Z_Down(float start_z, float dNum)
+        {
+            return start_z - dNum;
+        }
+
+        private static float Calculate_Z_Down2(float start_z, float dNum)
+        {
+            return start_z + dNum;
+        }
+
+        private static float Calculate_Z_Up2(float start_z, float dNum)
+        {
+            return start_z - dNum;
         }
     }
 }
