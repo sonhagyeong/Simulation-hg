f177d4948478bd318c11e2a23717cd2dbc105b6b
ppurify <qpqp8558@gmail.com>
2023-05-31 00:50:07 +0900

    Add StopVehicles Scripts

---
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/StopVehicles.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrafficSimulation{
+    public class StopVehicles : MonoBehaviour
+    {
+        [SerializeField] private static int vehiclesCount;
+        [SerializeField] private string nearStationName;
+
+        public float slowingTime = 3f; 
+        private float rightNum = 30f;
+
+        private float axis_z = 7.5f;
+
+        private GameObject vehicle;
+        private VehicleAI thisVehicleAI;
+
+        void Start()
+        {
+            GetStationName();
+        }
+
+        void OnTriggerEnter(Collider _other)
+        {   
+            GameObject nearStationOB = GameObject.Find("Stations/" + nearStationName);
+
+            vehiclesCount = nearStationOB.GetComponent<StationsInfo>().finishedVehicle_Count;
+            
+            vehicle = _other.gameObject;
+
+            thisVehicleAI = vehicle.GetComponent<VehicleAI>();
+
+
+            if(vehiclesCount > 0)
+            {
+                StartCoroutine(ReduceSpeed(vehicle, slowingTime));
+                thisVehicleAI.vehicleStatus = Status.STOP;
+            }
+            // vehiclesCount = StationsInfo.finishedVehicle_Count;
+
+            // Debug.Log("vehiclesCount: " + vehiclesCount);
+
+            // vehicle = _other.gameObject;
+            // thisVehicleAI = vehicle.GetComponent<VehicleAI>();
+
+            // if(vehiclesCount > 0)
+            // {
+            //     ReduceSpeed(vehicle, slowingTime);
+            //     thisVehicleAI.vehicleStatus = Status.STOP;
+            // }
+        }
+
+        private System.Collections.IEnumerator ReduceSpeed(GameObject _vehicle, float _slowingTime)
+        {   
+            UnityEngine.Debug.Log("ReduceSpeed");
+            Rigidbody rb = _vehicle.GetComponent<Rigidbody>();
+            
+            Vector3 initialVelocity = rb.velocity;
+            float elapsedTime = 0f;
+
+            while (elapsedTime < _slowingTime)
+            {
+                rb.velocity = Vector3.Lerp(initialVelocity, Vector3.zero, elapsedTime / _slowingTime);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            rb.velocity = Vector3.zero; // Ensure velocity is set to zero
+        }
+
+        private void GetStationName()
+        {
+            Vector3 stopPosition = this.transform.position;
+
+            if(stopPosition.z == -axis_z)
+            {   
+                Vector3 nearStationPos = new Vector3(stopPosition.x + rightNum, 0f, stopPosition.z + axis_z);
+                nearStationName = nearStationPos.ToString();
+            }
+
+            else
+            {
+                Vector3 nearStationPos = new Vector3(stopPosition.x - rightNum, 0f, stopPosition.z - axis_z);
+                nearStationName = nearStationPos.ToString();
+            }
+        }
+    }
+}
--- Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/Timer.cs
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/Timer.cs
@@ -12,19 +12,19 @@ namespace TrafficSimulation{
     public class Timer : MonoBehaviour
     {
         public float disappearDelay = 3f;
-        public float processTime = 8f;
-        public float slowingTime = 2f; 
+        // public float processTime = 8f;
+        // public float slowingTime = 3f; 
         public float speedTime = 10f;
         public float maxSpeed = 20f;
         
-        private GameObject vehicle;
-        private VehicleAI thisVehicleAI;
+        // private GameObject vehicle;
+        // private VehicleAI thisVehicleAI;
 
-        private Stopwatch totalWatch;
-        private Stopwatch stationWatch;
+        public Stopwatch totalWatch;
+        public Stopwatch stationWatch;
 
         // 파일 저장 위치
-        [SerializeField] private string filePath = "Assets/result.csv";
+        // [SerializeField] private string filePath = "Assets/result.csv";
 
         private string truckName;
         private string routeName;
@@ -34,15 +34,25 @@ namespace TrafficSimulation{
         private float arrivalTime;
         private float stationArrivalTime;
 
-        private List<Vector3> pickupPositions;
-        private List<Vector3> dropPositions;
-        [SerializeField] private Vector3 destinationPos;
+        public List<Vector3> pickupPositions;
+        public List<Vector3> dropPositions;
+        // [SerializeField] private Vector3 destinationPos;
+        public Vector3 destinationPos;
 
-        private List<GameObject> vehiclesInStation;
+
 
         private static List<CreateTruckData> truckData_List = CreateTruck.truckDataList;
         
-        private int stationCount;
+
+        // public static int finishedVehicle_Count;
+
+        public int processStatus;
+        // private float rigthToLeft = 15f;
+        // private float leftToRight = 30f;
+        // private float checkRange = 20f;
+
+        private Vector3 nowPosition;
+        // private Collider vehicleCollider;
 
         void Start()
         {
@@ -55,143 +65,106 @@ namespace TrafficSimulation{
             stationWatch.Start();
 
             GetTruckInformation(truckData_List);
-            vehiclesInStation = new List<GameObject>();
-            stationCount = 0;
+            processStatus = 0;
         }
+
 
         public void TimerStop(Stopwatch _watch)
         {
             _watch.Stop();
             // Convert milliseconds to minutes
             arrivalTime = _watch.ElapsedMilliseconds / 60000f; 
-            UnityEngine.Debug.Log(vehicle + "Time elapsed: " + arrivalTime + " min");
+            // UnityEngine.Debug.Log(vehicle + "Time elapsed: " + arrivalTime + " min");
         }
 
-        void OnTriggerEnter(Collider other)
-        {   
-            vehicle = this.gameObject;
-            truckName = vehicle.name;
+        // void OnTriggerEnter(Collider _other)
+        // {
+        //     vehicle = this.gameObject;
 
-            vehiclesInStation.Add(vehicle);
+        //     thisVehicleAI = vehicle.GetComponent<VehicleAI>();
+            
+        //     vehicleCollider = vehicle.GetComponent<Collider>();
 
-            // UnityEngine.Debug.Log("other.gameObject : " + other.gameObject.name);
-            thisVehicleAI = vehicle.GetComponent<VehicleAI>();
-            routeName = thisVehicleAI.trafficSystem.name;
+        //     // if(finishedVehicle_Count > 0 && vehicleStatus == 0)
+        //     // {
+        //     //     StartCoroutine(ReduceSpeed(vehicle, slowingTime));
+        //     //     thisVehicleAI.vehicleStatus = Status.STOP;
+        //     // }
 
-            // compare other.name and pickup station
-            if(stationCount == 0 &&(CompareStations(pickupPositions, other.name) || CompareStations(dropPositions, other.name)))
-            {
+        //     if(vehicleStatus == 0 && ( CompareStations(pickupPositions, _other.name) || CompareStations(dropPositions, _other.name)) )
+        //     {   
+        //         StartCoroutine(ReduceSpeed(vehicle, slowingTime));
+        //         thisVehicleAI.vehicleStatus = Status.STOP;
 
-                StartCoroutine(ReduceSpeed(vehicle, slowingTime));
-                thisVehicleAI.vehicleStatus = Status.STOP;
-
-                // disappear after desapperDelay seconds
-                Invoke("Disappear", disappearDelay);
-
-                vehiclesInStation.Remove(vehicle);
+        //         StartCoroutine(MoveToProcess(slowingTime + 1f));
                 
-
-                stationCount++;
+        //         vehicleStatus = 1;
                 
-                // destination이 아닌 station에 도착했을 때
-                if(!CompareDestination(destinationPos, other.name))
-                {
-                    Invoke("Appear", processTime);
-                }
+        //         // destination이 아닌 station에 도착했을 때
+        //         if(!CompareDestination(destinationPos, _other.name))
+        //         {
+        //             // Processing: Wait for the process time
+        //             StartCoroutine(Processing(processTime, _other));
+        //         }
 
-                // when vehicle arrives at the destination
-                else
-                {
-                    TimerStop(totalWatch);
-                    // save the result
-                    SaveToCSV(filePath, truckName, routeName, destinationPos, departureTime, arrivalTime);
-                    
-                }
-            }
-        }
+        //         // when vehicle arrives at the destination
+        //         else
+        //         {
+        //             TimerStop(totalWatch);
+        //         }
+        //     }
 
-        void OnTriggerExit(Collider _other)
-        {
-            vehiclesInStation.Remove(vehicle);
-            stationCount = 0;
-        }
+        // }
+
+        
+        // void OnTriggerExit(Collider _other)
+        // {
+        //     vehicleStatus = 0;
+        // }
 
 
         
         // private System.Collections.IEnumerator ReduceSpeed(GameObject _vehicle, float _slowingTime)
-        private System.Collections.IEnumerator ReduceSpeed(GameObject _vehicle, float _slowingTime)
-        {   
-            UnityEngine.Debug.Log("ReduceSpeed");
-            Rigidbody rb = _vehicle.GetComponent<Rigidbody>();
+        // private System.Collections.IEnumerator ReduceSpeed(GameObject _vehicle, float _slowingTime)
+        // {   
+        //     UnityEngine.Debug.Log("ReduceSpeed");
+        //     Rigidbody rb = _vehicle.GetComponent<Rigidbody>();
             
-            Vector3 initialVelocity = rb.velocity;
-            float elapsedTime = 0f;
+        //     Vector3 initialVelocity = rb.velocity;
+        //     float elapsedTime = 0f;
 
-            while (elapsedTime < _slowingTime)
-            {
-                rb.velocity = Vector3.Lerp(initialVelocity, Vector3.zero, elapsedTime / _slowingTime);
-                elapsedTime += Time.deltaTime;
-                yield return null;
-            }
+        //     while (elapsedTime < _slowingTime)
+        //     {
+        //         rb.velocity = Vector3.Lerp(initialVelocity, Vector3.zero, elapsedTime / _slowingTime);
+        //         elapsedTime += Time.deltaTime;
+        //         yield return null;
+        //     }
 
-            // thisVehicleAI.vehicleStatus = Status.STOP;
-            rb.velocity = Vector3.zero; // Ensure velocity is set to zero
-        }
+        //     // thisVehicleAI.vehicleStatus = Status.STOP;
+        //     rb.velocity = Vector3.zero; // Ensure velocity is set to zero
+        // }
 
-        private IEnumerator IncreaseSpeed(GameObject _vehicle, float _speedTime, float _maxSpeed)
-        {   
-            UnityEngine.Debug.Log("IncreaseSpeed");
-            thisVehicleAI.vehicleStatus = Status.GO;
+        // private IEnumerator IncreaseSpeed(GameObject _vehicle, float _speedTime, float _maxSpeed)
+        // {   
+        //     UnityEngine.Debug.Log("IncreaseSpeed");
+        //     thisVehicleAI.vehicleStatus = Status.GO;
             
-            Rigidbody rb = _vehicle.GetComponent<Rigidbody>();
-            // rb.centerOfMass = new Vector3(0f, -5f, 0f);
+        //     Rigidbody rb = _vehicle.GetComponent<Rigidbody>();
+        //     // rb.centerOfMass = new Vector3(0f, -5f, 0f);
 
-            Vector3 initialVelocity = rb.velocity;
-            float elapsedTime = 0f;
+        //     Vector3 initialVelocity = rb.velocity;
+        //     float elapsedTime = 0f;
 
-            while (elapsedTime < _speedTime)
-            {
-                // rb.velocity = Vector3.Lerp(Vector3.zero, initialVelocity, elapsedTime / _speedTime) + Vector3.forward * _maxSpeed;
-                rb.velocity = Vector3.Lerp(Vector3.zero, initialVelocity + Vector3.forward * _maxSpeed, elapsedTime / _speedTime);
-                elapsedTime += Time.deltaTime;
-                yield return null;
-            }
+        //     while (elapsedTime < _speedTime)
+        //     {
+        //         // rb.velocity = Vector3.Lerp(Vector3.zero, initialVelocity, elapsedTime / _speedTime) + Vector3.forward * _maxSpeed;
+        //         rb.velocity = Vector3.Lerp(Vector3.zero, initialVelocity + Vector3.forward * _maxSpeed, elapsedTime / _speedTime);
+        //         elapsedTime += Time.deltaTime;
+        //         yield return null;
+        //     }
 
-            rb.velocity = initialVelocity + Vector3.forward * _maxSpeed; // Ensure velocity reaches the desired speed
-        }
-
-        private void Disappear()
-        {   
-            if(vehicle != null)
-            {   
-                UnityEngine.Debug.Log(vehicle + " Disappear");
-                // Deactivate or destroy the object to make it disappear
-                vehicle.SetActive(false);
-                // Alternatively, you can destroy the object using Destroy(gameObject);
-            }
-
-            else
-            {
-                UnityEngine.Debug.LogError("No target object assigned.");
-            }
-        }
-
-        private void Appear()
-        {   
-            if(vehicle != null)
-            {
-                UnityEngine.Debug.Log(vehicle + " Appear");
-                vehicle.SetActive(true);
-                thisVehicleAI.vehicleStatus = Status.GO;
-                // StartCoroutine(IncreaseSpeed(vehicle, speedTime, maxSpeed));
-            }
-
-            else
-            {
-                UnityEngine.Debug.LogError("No target object assigned.");
-            }
-            
-        }
+        //     rb.velocity = initialVelocity + Vector3.forward * _maxSpeed; // Ensure velocity reaches the desired speed
+        // }
 
         private void GetTruckInformation(List<CreateTruckData> dataList)
         {
@@ -204,19 +177,109 @@ namespace TrafficSimulation{
             destinationPos = dropPositions.Last();
         }
         
-        private bool CompareStations(List<Vector3> stationList, string otherName)
-        {
-            // Convert the vectorList to a List<string> using LINQ
-            List<string> stationStrList = stationList.Select(v => v.ToString()).ToList();
-            return stationStrList.Contains(otherName);
-        }
+        // private bool CompareStations(List<Vector3> stationList, string otherName)
+        // {
+        //     // Convert the vectorList to a List<string> using LINQ
+        //     List<string> stationStrList = stationList.Select(v => v.ToString()).ToList();
+        //     return stationStrList.Contains(otherName);
+        // }
 
-        private bool CompareDestination(Vector3 destiPo, string otherName)
-        {
-            // Convert the vectorList to a List<string> using LINQ
-            string destiStr = destiPo.ToString();
-            return destiStr == otherName;
-        }
+        // private bool CompareDestination(Vector3 destiPo, string otherName)
+        // {
+        //     // Convert the vectorList to a List<string> using LINQ
+        //     string destiStr = destiPo.ToString();
+        //     return destiStr == otherName;
+        // }
+
+        // After moveDelay seconds, move to waiting area
+        // private IEnumerator MoveToProcess(float _moveDelay)
+        // {
+        //     yield return new WaitForSeconds(_moveDelay);
+
+        //     nowPosition = vehicle.transform.position;
+
+        //     vehicleCollider.enabled = false;
+
+        //     // Move to waiting area
+        //     if(vehicle.transform.rotation.y == 90f)
+        //     {   
+        //         UnityEngine.Debug.Log("this truck's rotation : 90, Move to waiting area");
+        //         vehicle.transform.position = nowPosition + new Vector3(0,0,rigthToLeft);
+        //     }
+
+        //     else
+        //     {
+        //         UnityEngine.Debug.Log("this truck's rotation : 90, Move to waiting area");
+        //         vehicle.transform.position = nowPosition + new Vector3(0,0,leftToRight);
+        //     }
+        // }
+
+
+        // // After processTime seconds, move to original position
+        // private IEnumerator Processing(float _processTime, Collider _other)
+        // {   
+        //     yield return new WaitForSeconds(_processTime);
+        //     UnityEngine.Debug.Log("Processing Done");
+            
+        //     // _other.gameObject.GetComponent<StationsInfo>().finishedVehicle_Count += 1;
+        //     // finishedVehicle_Count += 1;
+            
+        //     // The delay between checks for trucks
+        //     float checkDelay = 0.1f; 
+
+        //     while(ExistAnyTruck(checkRange))
+        //     {
+        //         yield return new WaitForSeconds(checkDelay);
+        //     }
+
+        //     // Move to original position
+        //     UnityEngine.Debug.Log("Move to original position");
+
+        //     // _other.gameObject.GetComponent<StationsInfo>().finishedVehicle_Count -= 1;
+        //     // finishedVehicle_Count -= 1;
+        //     // UnityEngine.Debug.Log(_other.name + " finishedVehicle_Count : " + _other.gameObject.GetComponent<StationsInfo>().finishedVehicle_Count);
+
+        //     // Return to original position 
+        //     vehicle.transform.position = nowPosition;
+        //     vehicleCollider.enabled = true;
+
+        //     thisVehicleAI.vehicleStatus = Status.GO;
+        // }
+        
+        
+
+        // private bool ExistAnyTruck(float _checkRange)
+        // {
+        //     Vector3 currentPosition = this.transform.position;
+        //     // The range on the x-axis to check for vehicles
+
+        //     // Perform a raycast to check for vehicles
+        //     RaycastHit hit;
+
+        //     bool rightSide = Physics.Raycast(currentPosition, Vector3.right, out hit, _checkRange);
+        //     bool leftSide = Physics.Raycast(currentPosition, Vector3.left, out hit, _checkRange);
+
+        //     return rightSide || leftSide;
+        // }
+
+        // private IEnumerator WaitForTruckClear()
+        // {
+        //     UnityEngine.Debug.Log("WaitForTruckClear");
+        //     // The delay between checks for trucks
+        //     float checkDelay = 0.1f;
+
+        //     UnityEngine.Debug.Log("ExistAnyTruck : " + ExistAnyTruck());
+        //     while(ExistAnyTruck())
+        //     {
+        //         yield return new WaitForSeconds(checkDelay);
+        //     }
+
+        //     // No trucks detected, activate the object
+        //     vehicle.SetActive(true);
+        //     thisVehicleAI.vehicleStatus = Status.GO;
+        //     UnityEngine.Debug.Log(vehicle + " Appear");
+        // }
+
 
         private void SaveToCSV(string _filePath, string _truckName, string _routeName, Vector3 _destination, float _departureTime, float _arrivalTime)
         {
