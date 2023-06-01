afff8e2f32e161cdd4dbe27550d24226d4a9e6e0
ppurify <qpqp8558@gmail.com>
2023-06-01 23:35:34 +0900

    Add function when vehicle has same start position

--- Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/CreateTruckAndStation.cs
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/CreateTruckAndStation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Text.RegularExpressions;
+using System;
 
 namespace TrafficSimulation{
     public class CreateTruckAndStation : MonoBehaviour
@@ -20,15 +21,19 @@ namespace TrafficSimulation{
         private static Vector3 stationSize = new Vector3(50,10,30);
         private static float stationPos_y = stationSize.y/2;
 
+        private static Dictionary<Vector3, List<Tuple<string, string, List<Vector3>>>> startPositionDict;
+
         // Start is called before the first frame update
         void Start()
         {
             ReadFile(truckFilePath);
             CreateStations(truckDataList);
             if(ExistRoute(truckDataList))
-            {
+            {   
+                IsDuplicateStartPosition(truckDataList);
+                CreateTrucks(startPositionDict);
                 Debug.Log("All routes exist");
-                CreateTrucks(truckDataList);
+                // CreateTrucks(truckDataList);
             }
         }
 
@@ -108,48 +113,6 @@ namespace TrafficSimulation{
         }
 
 
-        public static void CreateTrucks(List<CreateTruckData> dataList)
-        {
-            foreach (CreateTruckData data in dataList)
-            {   
-                // Generate a random number between 1 and 4 (inclusive)
-                // int randomNumber = Random.Range(1, 5);
-                // string truckPrefabName = "Truck" + randomNumber.ToString();
-                string truckPrefabName = "Truck1";
-                string routeName = "Route-" + data.Route;
-                GameObject truckPrefab = Resources.Load(truckPrefabName) as GameObject;
-
-                if (truckPrefab != null)
-                {
-                    GameObject truck = Instantiate(truckPrefab);
-                    truck.name = data.Name;
-                    Transform routeTransform = GameObject.Find(routeName).transform;
-
-                    Vector3 routePosition = routeTransform.Find(routeName + "/Waypoint-0").transform.position;
-                    truck.transform.position = new Vector3(routePosition.x, 0f, routePosition.z);
-                    truck.transform.rotation = Quaternion.Euler(0, GetTruckRotation(routeTransform, routeName), 0);
-
-                    // Set the truck's route
-                    truck.GetComponent<VehicleAI>().trafficSystem = GameObject.Find(routeName).GetComponent<TrafficSystem>();
-
-                    // Set the truck's work stations
-                    // TruckInfo truckInfo = truck.AddComponent<TruckInfo>();
-                    TruckInfo truckInfo = truck.GetComponent<TruckInfo>();
-
-                    truckInfo.truckWorkStations = data.WorkStations;
-
-                    int workStationCount = data.WorkStations.Count;
-                    truckInfo.destination = data.WorkStations[workStationCount - 1];
-                }
-
-                else
-                {
-                    Debug.LogError("Truck prefab not found: " + truckPrefabName);
-                }
-
-            }
-        }
-
         public static float GetTruckRotation(Transform routeTransform, string routeName)
         {
             Vector3 position_1 = routeTransform.Find(routeName + "/Waypoint-0").transform.position;
@@ -188,6 +151,7 @@ namespace TrafficSimulation{
             return truckRotation_y;
         }
 
+        
         public static void CreateStations(List<CreateTruckData> dataList)
         {   
             GameObject stationsOB = GameObject.Find("Stations");
@@ -229,6 +193,7 @@ namespace TrafficSimulation{
             }
         }
 
+        
         public static bool ExistStation(string parentOBName, string stationName)
         {   
             bool isExist = true;
@@ -240,6 +205,7 @@ namespace TrafficSimulation{
             return isExist;
         }
 
+        
         public static void AddCollider(GameObject obj)
         {
             BoxCollider bc = obj.AddComponent<BoxCollider>();
@@ -250,5 +216,148 @@ namespace TrafficSimulation{
             bc.isTrigger = true;
         }
         
+        
+        private static void IsDuplicateStartPosition(List<CreateTruckData> dataList)
+        {
+            // startPositionDict = new Dictionary<Vector3, List<List<string>>>();
+            startPositionDict = new Dictionary<Vector3, List<Tuple<string, string, List<Vector3>>>>();
+
+            foreach(CreateTruckData data in dataList)
+            {   
+                string parentName = "Route-" + data.Route;
+                string childName = parentName + "/Waypoint-0";
+                
+                GameObject parentOB = GameObject.Find(parentName);
+
+                if(parentOB != null)
+                {
+                    Transform childTransform = parentOB.transform.Find(childName);
+
+                    if(childTransform != null)
+                    {
+                        Vector3 startPoint = childTransform.position;
+
+                        // List<string> nameAndRoute = new List<string> {data.Name, parentName};
+                        Tuple<string, string, List<Vector3>> _truckData = Tuple.Create(data.Name, parentName, data.WorkStations);
+
+                        if(startPositionDict.ContainsKey(startPoint))
+                        {
+                            // startPositionDict[startPoint].Add(data.Route);
+                            
+                            startPositionDict[startPoint].Add(_truckData);
+
+                            // startPositionDict[startPoint].Add(data.Name);
+
+                        }
+
+                        else
+                        {
+                            startPositionDict[startPoint] = new List<Tuple<string, string, List<Vector3>>> { _truckData };
+
+                            // startPositionDict[startPoint] = new List<string> { data.Name };
+                        }
+                    }
+
+                    else
+                    {
+                        Debug.LogError("Child object not found: " + childName);
+                    }
+                }
+
+                else
+                {
+                    Debug.LogError("Parent object not found: " + parentName);
+                }
+            }
+        }
+
+        
+        private void CreateTrucks(Dictionary<Vector3, List<Tuple<string, string, List<Vector3>>>> _dictionary)
+        {
+            // Print the duplicate start positions
+            foreach (KeyValuePair<Vector3, List<Tuple<string, string, List<Vector3>>>> kvp in _dictionary)
+            {   
+                // Debug.Log("kvp.Value.Count: " + kvp.Value.Count);
+
+                Vector3 key = kvp.Key;
+                List<Tuple<string, string, List<Vector3>>> values = kvp.Value;
+
+                // 출발 위치가 동일한 트럭이 있는 경우
+                if(values.Count > 1)
+                {
+                    float createDelay = 5f;
+                    StartCoroutine(DuplicatePositionCreateTruck(values, createDelay));
+                }
+
+                // 출발 위치가 동일한 트럭이 없는 경우
+                else
+                {
+                    string truckName = values[0].Item1;
+                    string routeName = values[0].Item2;
+                    List<Vector3> truckWorkStations = values[0].Item3;
+
+                    CreateTruck(truckName, routeName, truckWorkStations);
+                }
+                
+                
+           
+            }
+        }
+        
+        
+        private void CreateTruck(string _truckName, string _routeName, List<Vector3> _workStaions)
+        {
+            // Generate a random number between 1 and 4 (inclusive)
+            int randomNumber = UnityEngine.Random.Range(1, 5);
+            string truckPrefabName = "Truck" + randomNumber.ToString();
+            // string truckPrefabName = "Truck1";
+            // string routeName = "Route-" + data.Route;
+            GameObject truckPrefab = Resources.Load(truckPrefabName) as GameObject;
+
+            if (truckPrefab != null)
+            {
+                GameObject truck = Instantiate(truckPrefab);
+                truck.name = _truckName;
+                Transform routeTransform = GameObject.Find(_routeName).transform;
+
+                Vector3 routePosition = routeTransform.Find(_routeName + "/Waypoint-0").transform.position;
+                truck.transform.position = new Vector3(routePosition.x, 0f, routePosition.z);
+                truck.transform.rotation = Quaternion.Euler(0, GetTruckRotation(routeTransform, _routeName), 0);
+
+                // Set the truck's route
+                truck.GetComponent<VehicleAI>().trafficSystem = GameObject.Find(_routeName).GetComponent<TrafficSystem>();
+
+                // Set the truck's work stations
+                // TruckInfo truckInfo = truck.AddComponent<TruckInfo>();
+                TruckInfo truckInfo = truck.GetComponent<TruckInfo>();
+
+                truckInfo.truckWorkStations = _workStaions;
+
+                int workStationCount = _workStaions.Count;
+                truckInfo.destination = _workStaions[workStationCount - 1];
+            }
+
+            else
+            {
+                Debug.LogError("Truck prefab not found: " + truckPrefabName);
+            }
+        }
+
+        
+        private IEnumerator DuplicatePositionCreateTruck(List<Tuple<string, string, List<Vector3>>> _values, float _createDelay)
+        {
+            foreach (Tuple<string, string, List<Vector3>> value in _values)
+            {
+                string truckName = value.Item1;
+                string routeName = value.Item2;
+                List<Vector3> truckWorkStations = value.Item3;
+
+                Debug.Log("Create Duplicate Position Truck: " + truckName + ", " + routeName);
+                CreateTruck(truckName, routeName, truckWorkStations);
+
+                yield return new WaitForSeconds(_createDelay);
+            }
+        }
+    
     }
 }
