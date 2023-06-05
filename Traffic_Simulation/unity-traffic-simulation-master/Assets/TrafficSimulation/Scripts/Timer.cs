c73ae8cba6b34b5b2a72f0ef1079304864586878
ppurify <qpqp8558@gmail.com>
2023-06-05 23:14:04 +0900

    delete namespace

--- Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/Timer.cs
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/Timer.cs
@@ -7,104 +7,94 @@ using UnityEngine;
 using System.Linq;
 
 
+public class Timer : MonoBehaviour
+{
+    // 파일 저장 위치
+    public string filePath = "Assets/result.csv";
 
-namespace TrafficSimulation{
-    public class Timer : MonoBehaviour
-    {
-        // 파일 저장 위치
-        public string filePath = "Assets/result.csv";
+    // StopWatch
+    public Stopwatch totalWatch;
+    public Stopwatch stationWatch;
 
-        // StopWatch
-        public Stopwatch totalWatch;
-        public Stopwatch stationWatch;
-
-        public List<float> stationWatchList;
-        public float totalTime;
-
-
-        // // Truck Information
-        // private TruckInfo truckInfo;
-        // private Vector3 truckOrigin;
-        // private Vector3 truckDestination;
-        
-        
+    public List<float> stationWatchList;
+    public float totalTime;
     
-        void Start()
-        {
-            totalWatch = new Stopwatch();
-            stationWatch = new Stopwatch();
-            
-            stationWatchList = new List<float>();
-
-            // Get truck origin and destination
-            // truckInfo = this.gameObject.GetComponent<TruckInfo>();
-            // GetTruckInformation(truckInfo);
-            // departureTime = totalWatch.ElapsedMilliseconds / 1000f;
-
-            totalWatch.Start();
-            stationWatch.Start();
-        }
-
-        public float TimerStop(Stopwatch _watch)
-        {
-            _watch.Stop();
-            // Convert milliseconds to minutes
-            // arrivalTime = _watch.ElapsedMilliseconds / 60000f; 
-
-            return _watch.ElapsedMilliseconds / 1000f;
-        }
-
-
-        // private void GetTruckInformation(TruckInfo _truckInfo)
-        // {
-        //     truckOrigin = _truckInfo.origin;
-        //     truckDestination = _truckInfo.destination;
-        // }
+    
+    void Start()
+    {
+        totalWatch = new Stopwatch();
+        stationWatch = new Stopwatch();
         
-       
+        stationWatchList = new List<float>();
 
-        public void SaveToCSV(string _filePath, string _truckName, string _routeName, Vector3 _origin, Vector3 _destination, float _totalTime, List<float> _arrivalTimeList)
-        {
-            // Check if the CSV file exists
-            if(!File.Exists(_filePath))
-            {
-                // Create a new CSV file and write the data
-                using (StreamWriter sw = File.CreateText(_filePath))
-                {
-                    string header = "Truck_id, Route_id, Origin, Destination, Total Time, Arrival Time";
+        // Get truck origin and destination
+        // truckInfo = this.gameObject.GetComponent<TruckInfo>();
+        // GetTruckInformation(truckInfo);
+        // departureTime = totalWatch.ElapsedMilliseconds / 1000f;
 
-                    // Write the header and data to the CSV file
-                    sw.WriteLine(header);
-                    // sw.WriteLine(newLine);
-                }
-            }
-
-            // Read the existing content of the CSV file
-            string[] lines = File.ReadAllLines(_filePath);
-
-            // Convert the List<float> to a comma-separated string
-            string arrivalTimeValues = string.Join(",", _arrivalTimeList);
-            UnityEngine.Debug.Log("arrivalTimeValues: " + arrivalTimeValues);
-
-            // Convert the Vector3 values to strings without including commas
-            string originValue = _origin.ToString().Replace(",", string.Empty);
-            string destinationValue = _destination.ToString().Replace(",", string.Empty);
-            // string originValue = $"({_origin.x},{_origin.y},{_origin.z})";
-            // string destinationValue = $"({_destination.x},{_destination.y},{_destination.z})";
-
-            // Append the new data to the content
-            string newLine = string.Format("{0},{1},{2},{3},{4},{5}", _truckName, _routeName, originValue, destinationValue, _totalTime, arrivalTimeValues);
-
-            // Append the new line to the CSV file
-            File.AppendAllText(_filePath, newLine + "\n");
-            
-            // string updatedContent = string.Join("\n", lines) + "\n" + newLine;
-
-            // // Write the updated content back to the CSV file
-            // File.WriteAllText(_filePath, updatedContent);
-
-        }
-
-        
+        totalWatch.Start();
+        stationWatch.Start();
     }
+
+    public float TimerStop(Stopwatch _watch)
+    {
+        _watch.Stop();
+        // Convert milliseconds to minutes
+        // arrivalTime = _watch.ElapsedMilliseconds / 60000f; 
+
+        return _watch.ElapsedMilliseconds / 1000f;
+    }
+
+
+    // private void GetTruckInformation(TruckInfo _truckInfo)
+    // {
+    //     truckOrigin = _truckInfo.origin;
+    //     truckDestination = _truckInfo.destination;
+    // }
+    
+    
+
+    public void SaveToCSV(string _filePath, string _truckName, string _routeName, Vector3 _origin, Vector3 _destination, float _totalTime, List<float> _arrivalTimeList)
+    {
+        // Check if the CSV file exists
+        if(!File.Exists(_filePath))
+        {
+            // Create a new CSV file and write the data
+            using (StreamWriter sw = File.CreateText(_filePath))
+            {
+                string header = "Truck_id, Route_id, Origin, Destination, Total Time, Arrival Time";
+
+                // Write the header and data to the CSV file
+                sw.WriteLine(header);
+                // sw.WriteLine(newLine);
+            }
+        }
+
+        // Read the existing content of the CSV file
+        string[] lines = File.ReadAllLines(_filePath);
+
+        // Convert the List<float> to a comma-separated string
+        string arrivalTimeValues = string.Join(",", _arrivalTimeList);
+        UnityEngine.Debug.Log("arrivalTimeValues: " + arrivalTimeValues);
+
+        // Convert the Vector3 values to strings without including commas
+        string originValue = _origin.ToString().Replace(",", string.Empty);
+        string destinationValue = _destination.ToString().Replace(",", string.Empty);
+        // string originValue = $"({_origin.x},{_origin.y},{_origin.z})";
+        // string destinationValue = $"({_destination.x},{_destination.y},{_destination.z})";
+
+        // Append the new data to the content
+        string newLine = string.Format("{0},{1},{2},{3},{4},{5}", _truckName, _routeName, originValue, destinationValue, _totalTime, arrivalTimeValues);
+
+        // Append the new line to the CSV file
+        File.AppendAllText(_filePath, newLine + "\n");
+        
+        // string updatedContent = string.Join("\n", lines) + "\n" + newLine;
+
+        // // Write the updated content back to the CSV file
+        // File.WriteAllText(_filePath, updatedContent);
+
+    }
+
+    
 }
