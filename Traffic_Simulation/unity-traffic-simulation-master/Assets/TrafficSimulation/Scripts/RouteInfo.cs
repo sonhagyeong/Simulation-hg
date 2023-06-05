9840c2f325331821cbdf32f5c83c3675bcec4c53
ppurify <qpqp8558@gmail.com>
2023-06-05 18:04:08 +0900

    route Information about turn time

---
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/RouteInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouteInfo : MonoBehaviour
+{
+    [Tooltip("The number of times the vehicle should make a U-turn")]
+    public int uTurnNum;
+    
+}
