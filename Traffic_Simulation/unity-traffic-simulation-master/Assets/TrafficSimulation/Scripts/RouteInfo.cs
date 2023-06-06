d883448c243607251659488a6267e1763b0862d1
ppurify <qpqp8558@gmail.com>
2023-06-07 00:05:43 +0900

    delete uTurnNum and add uTurnStations

--- Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/RouteInfo.cs
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/RouteInfo.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class RouteInfo : MonoBehaviour
 {
-    [Tooltip("The number of times the vehicle should make a U-turn")]
-    public int uTurnNum;
+    // [Tooltip("The number of times the vehicle should make a U-turn")]
+    // public int uTurnNum;
+    public List<Vector3> uTurnStations = new List<Vector3>();
     
 }
