9e5c8c5e4211b11dfc5530ad70f7862e294716f4
ppurify <qpqp8558@gmail.com>
2023-06-12 23:22:20 +0900

    Add coner script

---
+++ Traffic_Simulation/unity-traffic-simulation-master/Assets/TrafficSimulation/Scripts/Corner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrafficSimulation{
+    public class Corner : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void OnTriggerEnter(Collider other)
+        {
+            VehicleAI vehicleAI = other.GetComponent<VehicleAI>();
+            other.GetComponent<TruckInfo>().nowStatus = NowStatus.WAITING;
+            vehicleAI.vehicleStatus = Status.SLOW_DOWN;
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            other.GetComponent<VehicleAI>().vehicleStatus = Status.GO;
+            other.GetComponent<TruckInfo>().nowStatus = NowStatus.NONE;
+
+        }
+    }
+}
